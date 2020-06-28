25116be61834b34d2d2fb92f9316aa9b404ab26b
Black995 <54086005+Black995@users.noreply.github.com>
2020-06-28 19:23:15 -0400

    Tablas de SQL

--- servidor/SQL/database.sql
+++ servidor/SQL/database.sql
@@ -5,230 +5,139 @@ CREATE DATABASE sistema_vam;
 --Listar tablas de una BD: \dt
 
 CREATE TABLE MP_PROHIBIDA(
-   cas INT PRIMARY KEY,
-   nombre VARCHAR NOT NULL
+   cas NUMERIC(12) PRIMARY KEY,
+   nombre VARCHAR(50) NOT NULL
 );
 
+--Está en "veremos"
 CREATE TABLE CONTINENTE(
    id SERIAL PRIMARY KEY,
-   nombre VARCHAR NOT NULL UNIQUE
+   nombre VARCHAR(10) NOT NULL UNIQUE
 );
 
---Falta colocarle el PK compuesta con continente
+
 CREATE TABLE PAIS(
    id SERIAL PRIMARY KEY,
-   nombre VARCHAR NOT NULL UNIQUE,
-   id_continente INT,
-   FOREIGN KEY(id_continente) REFERENCES CONTINENTE(id) ON DELETE SET NULL
+   nombre VARCHAR(20) NOT NULL UNIQUE,
+   continente VARCHAR(2) NOT NULL,
+   CONSTRAINT check_cont CHECK(continente in ('am', 'eu', 'af', 'as', 'oc'))
 );
 
+--Falta colocarle el FK con la compuesta con Pais
 CREATE TABLE PRODUCTOR(
    id SERIAL PRIMARY KEY,
-   nombre VARCHAR NOT NULL,
-   email VARCHAR NOT NULL UNIQUE,
-   telefono BIGINT NOT NULL UNIQUE,
-   pag_web VARCHAR NOT NULL UNIQUE,
-   local_nro INT NOT NULL UNIQUE,
-   calle VARCHAR NOT NULL,
-   cod_postal INT NOT NULL,
-   id_pais INT,
-   FOREIGN KEY(id_pais) REFERENCES PAIS(id) ON DELETE SET NULL
+   nombre VARCHAR(50) NOT NULL,
+   email VARCHAR(50) NOT NULL UNIQUE,
+   telefono NUMERIC(10) NOT NULL UNIQUE,
+   pag_web VARCHAR(100) NOT NULL UNIQUE
+   --id_pais INT,
+   --CONSTRAINT id_pais FOREIGN KEY(id_pais) REFERENCES PAIS(id) ON DELETE SET NULL
 );
 
 CREATE TABLE PROVEEDOR(
    id SERIAL PRIMARY KEY,
-   nombre VARCHAR NOT NULL,
-   email VARCHAR NOT NULL UNIQUE,
-   telefono BIGINT NOT NULL UNIQUE,
-   pag_web VARCHAR NOT NULL UNIQUE,
-   local_nro INT NOT NULL UNIQUE,
-   calle VARCHAR NOT NULL,
-   cod_postal INT NOT NULL,
+   nombre VARCHAR(50) NOT NULL,
+   email VARCHAR(50) NOT NULL UNIQUE,
+   telefono NUMERIC(10) NOT NULL UNIQUE,
+   pag_web VARCHAR(100) NOT NULL UNIQUE,
    id_pais INT,
-   FOREIGN KEY(id_pais) REFERENCES PAIS(id) ON DELETE SET NULL
+   CONSTRAINT id_pais FOREIGN KEY(id_pais) REFERENCES PAIS(id) ON DELETE SET NULL
 );
 
 --Falta colocarle el PK compuesta con PROVEEDOR
 CREATE TABLE FORMA_ENVIO(
-   id SERIAL PRIMARY KEY,
-   tipo VARCHAR NOT NULL CHECK(tipo = 'm' OR tipo = 'a' OR tipo = 'a'),
-   cargo REAL NOT NULL,
+   id SERIAL,
+   id_prov INT,
+   tipo VARCHAR(1) NOT NULL CHECK(tipo = 'm' OR tipo = 'a' OR tipo = 'a'),
+   cargo NUMERIC(5,2) NOT NULL,
    id_pais INT,
-   id_cont_pais INT,
-   FOREIGN KEY(id_pais) REFERENCES PAIS(id) ON DELETE SET NULL,
-   FOREIGN KEY(id_cont_pais) REFERENCES CONTINENTE(id) ON DELETE SET NULL
+   CONSTRAINT id_pais FOREIGN KEY(id_pais) REFERENCES PAIS(id) ON DELETE SET NULL,
+   CONSTRAINT id_prov FOREIGN KEY(id_prov) REFERENCES PROVEEDOR(id),
+   CONSTRAINT id_forma_pais PRIMARY KEY(id, id_prov)
 );
 
---De aquí para arriba se tiene que modificar la PK compuesta con país
 
---Falta colocarle el PK compuesta con PROVEEDOR
+-- Cambiarlo de acuerdo a la corrección del mates
 CREATE TABLE FORMA_PAGO(
    id SERIAL PRIMARY KEY,
-   tipo VARCHAR NOT NULL CHECK(tipo = 'cont' OR tipo = 'cred'),
-   porc_inicial REAL,
-   nro_cuotas REAL,
-   interes_mensual REAL,
-   nro_dia_entre_pago INT,
-   id_proveedor INT,
-   FOREIGN KEY(id_proveedor) REFERENCES PROVEEDOR(id) ON DELETE SET NULL
+   tipo VARCHAR(4) NOT NULL CHECK(tipo = 'cont' OR tipo = 'cred'),
+   porc_inicial NUMERIC(5,2),
+   nro_cuotas NUMERIC(3),
+   interes_mensual NUMERIC(5,2),
+   nro_dia_entre_pago NUMERIC(3),
+   id_prov INT,
+   CONSTRAINT id_prov FOREIGN KEY(id_prov) REFERENCES PROVEEDOR(id) ON DELETE SET NULL
 );
 
+--Revisar si lleva el atributo tipo (después de la reunión con la profe)
+CREATE TABLE INGREDIENTE_ESENCIAS(
+   cas NUMERIC(12),
+   id_proveedor INT,
+   nombre VARCHAR(50) NOT NULL,
+   descripcion VARCHAR(400) NOT NULL,
+   taxonomia VARCHAR(50) NOT NULL,
+   punto_ebul NUMERIC(4),
+   punto_inflam NUMERIC(4),
+   proc_extrac VARCHAR(400),
+   descrip_proceso VARCHAR(400),
+   CONSTRAINT id_prov FOREIGN KEY(id_proveedor) REFERENCES PROVEEDOR(id) ON DELETE SET NULL,
+   CONSTRAINT pk_ingred PRIMARY KEY(cas, id_prov)
+);
 
---INSERT PARA MP_PROHIBIDA
-insert into MP_PROHIBIDA values(103640, 'Bromostyrene');
-insert into MP_PROHIBIDA values(103797, 'Phenyl acetone');
-insert into MP_PROHIBIDA values(104278, 'α-Methyl anisylidene acetone');
-insert into MP_PROHIBIDA values(108883, 'Toluene');
-insert into MP_PROHIBIDA values(109864, 'Ethylene glycol monomethyl ether');
-insert into MP_PROHIBIDA values(110496, 'Ethylene glycol monomethyl acetate');
-insert into MP_PROHIBIDA values(110805, 'Ethylene glycol monoethyl ether');
-insert into MP_PROHIBIDA values(111159, 'Ethylene glycol monoethyl acetate');
-insert into MP_PROHIBIDA values(111284, '2,4-Hexadien-1-ol');
-insert into MP_PROHIBIDA values(17102646,'2,4-Hexadien-1-ol');
-insert into MP_PROHIBIDA values(116665, 'Musk moskene');
-insert into MP_PROHIBIDA values(122394, 'Diphenylamine');
+CREATE TABLE CONTRATO(
+   id NUMERIC(12),
+   id_proveedor INT,
+   id_productor INT,
+   fecha_emision DATE NOT NULL,
+   clausula VARCHAR(1000),
+   exclusividad BOOLEAN NOT NULL,
+   motivo_cancel VARCHAR(400),
+   fecha_cancelacion DATE,
+   quien_cancela VARCHAR(50),
+   CONSTRAINT pk_contrato PRIMARY KEY(id,id_proveedor,id_productor)
+);
 
-insert into MP_PROHIBIDA values(122576, 'Benzylidene acetone');
-insert into MP_PROHIBIDA values(13393936, 'Hydroabietyl alcohol, Dihydroabietyl alcohol');
-insert into MP_PROHIBIDA values(26266773, 'Hydroabietyl alcohol, Dihydroabietyl alcohol');
-insert into MP_PROHIBIDA values(1333897, 'Hydroabietyl alcohol, Dihydroabietyl alcohol');
-insert into MP_PROHIBIDA values(13706860, 'Acetyl isovaleryl');
-insert into MP_PROHIBIDA values(140294, 'Benzyl cyanide');
-insert into MP_PROHIBIDA values(140885, 'Ethyl acrylate');
-insert into MP_PROHIBIDA values(141059, 'Diethyl maleate');
-insert into MP_PROHIBIDA values(141106, 'Pseudoionone');
-insert into MP_PROHIBIDA values(145391, 'Musk tibetene');
-insert into MP_PROHIBIDA values(150765, 'Hydroquinone monomethyl ether');
-insert into MP_PROHIBIDA values(17874349, '4,6-Dimethyl-8-tert-butylcoumarin');
-insert into MP_PROHIBIDA values(18318837, 'trans-2-Hexenal dimethyl acetal');
-insert into MP_PROHIBIDA values(18485386, '2,4-Dodecadien-1-ol, (2E, 4E)');
-insert into MP_PROHIBIDA values(18829555, 'trans-2-Heptenal');
-insert into MP_PROHIBIDA values(2445832, '7-Methylcoumarin');
-insert into MP_PROHIBIDA values(25564221, 'Amylcyclopentenone');
-insert into MP_PROHIBIDA values(25677401, '2-Pentylidene cyclohexanone');
-insert into MP_PROHIBIDA values(26651967,'Pseudo methylionones');
-insert into MP_PROHIBIDA values(72968253, 'Pseudo methylionones ');
-insert into MP_PROHIBIDA values(1117415, 'Pseudo methylionones ');
-insert into MP_PROHIBIDA values(33204749, 'Carvone oxide');
-insert into MP_PROHIBIDA values(34131992, '6-Isopropyl-2-decalol');
-insert into MP_PROHIBIDA values(3591422, '2,2-Dichloro-1-methylcyclopropylbenzene');
-insert into MP_PROHIBIDA values(40607485, '3,7-Dimethyl-2-octen-1-ol');
-insert into MP_PROHIBIDA values(4173448, 'Cinnamylidene acetone');
+ALTER TABLE CONTRATO ALTER COLUMN exclusividad
+SET DEFAULT FALSE;
 
-insert into MP_PROHIBIDA values(4756198, 'Cyclamen alcohol');
-insert into MP_PROHIBIDA values(494406, 'cis-and trans-Asarone');
-insert into MP_PROHIBIDA values(2883989, 'cis-and trans-Asarone');
-insert into MP_PROHIBIDA values(5273869, 'cis-and trans-Asarone');
-insert into MP_PROHIBIDA values(5146667, 'Geranyl nitrile');
-insert into MP_PROHIBIDA values(5585397, 'Geranyl nitrile');
-insert into MP_PROHIBIDA values(31983274, 'Geranyl nitrile');
+CREATE TABLE RENOVACION(
+   id SERIAL,
+   id_contrato INT,
+   id_proveedor INT,
+   id_productor INT,
+   fecha DATE,
+   CONSTRAINT fk_renov FOREIGN KEY(id_contrato, id_proveedor, id_productor) REFERENCES CONTRATO(id, id_proveedor, id_productor) ON DELETE SET NULL,
+   CONSTRAINT pk_renov PRIMARY KEY(id, id_contrato, id_proveedor, id_productor)
+);
 
-insert into MP_PROHIBIDA values(531599, '7-Methoxycoumarin');
-insert into MP_PROHIBIDA values(53153665, '3-Methyl-2(3)-nonenenitrile');
-insert into MP_PROHIBIDA values(5406122, 'p-Methylhydrocinnamic aldehyde');
-insert into MP_PROHIBIDA values(54814641, 'Massoia lactone');
-insert into MP_PROHIBIDA values(51154962, 'Massoia lactone');
-insert into MP_PROHIBIDA values(57067, 'Allyl isothiocyanate');
-insert into MP_PROHIBIDA values(617549, 'Dimethyl citraconate');
-insert into MP_PROHIBIDA values(622628, 'Hydroquinone monoethyl ether');
-insert into MP_PROHIBIDA values(62265990, 'Musk KS');
-insert into MP_PROHIBIDA values(623154, 'Furfurylidene acetone');
-insert into MP_PROHIBIDA values(623438, 'Methyl crotonate');
-insert into MP_PROHIBIDA values(6248200, '2,4-Dihydroxy-3-methylbenzaldehyde');
-insert into MP_PROHIBIDA values(63697530, 'Musk α');
-insert into MP_PROHIBIDA values(67746309, 'trans-2-Hexenal diethyl acetal');
-insert into MP_PROHIBIDA values(68916529, 'Fig leaf absolute');
-insert into MP_PROHIBIDA values(90028743, 'Fig leaf absolute');
-insert into MP_PROHIBIDA values(700823, 'Hexahydrocoumarin');
+CREATE TABLE PEDIDO(
+   id SERIAL PRIMARY KEY,
+   f_emision DATE NOT NULL,
+   estado VARCHAR(2) NOT NULL,
+   f_confirmacion DATE NOT NULL,
+   motivo_cancel VARCHAR(400),
+   nro_factura NUMERIC(3),
+   total_usd NUMERIC(9,2)
+);
 
-insert into MP_PROHIBIDA values(71432, 'Benzene');
-insert into MP_PROHIBIDA values(73157434, 'Allyl heptine carbonate');
-insert into MP_PROHIBIDA values(76299, '3-Bromo-1,7,7-trimethylbicyclo[2.2.1]heptane-2-one');
-insert into MP_PROHIBIDA values(764409, '2,4-Dienals');
-insert into MP_PROHIBIDA values(142836, '2,4-Dienals');
-insert into MP_PROHIBIDA values(80466348, '2,4-Dienals');
-insert into MP_PROHIBIDA values(5910850, '2,4-Dienals');
-insert into MP_PROHIBIDA values(30361285, '2,4-Dienals');
-insert into MP_PROHIBIDA values(6750034, '2,4-Dienals');
-insert into MP_PROHIBIDA values(2363884, '2,4-Dienals');
-insert into MP_PROHIBIDA values(13162464, '2,4-Dienals');
-insert into MP_PROHIBIDA values(21662168, '2,4-Dienals');
-insert into MP_PROHIBIDA values(25152845, '2,4-Dienals');
-insert into MP_PROHIBIDA values(30361296, '2,4-Dienals');
-insert into MP_PROHIBIDA values(4313035, '2,4-Dienals');
-insert into MP_PROHIBIDA values(20432400, '2,4-Dienals');
-insert into MP_PROHIBIDA values(4488486, '2,4-Dienals');
-insert into MP_PROHIBIDA values(5577446, '2,4-Dienals');
-insert into MP_PROHIBIDA values(5910872, '2,4-Dienals');
-insert into MP_PROHIBIDA values(78591, 'Isophorone');
-insert into MP_PROHIBIDA values(80546, 'p-tert-Butyl-α-methylhydrocinnamic aldehyde (p-BMHCA)');
-insert into MP_PROHIBIDA values(80626, 'Methyl methacrylate');
-insert into MP_PROHIBIDA values(8001-88-5, 'Birch wood pyrolysate');
-insert into MP_PROHIBIDA values(68917-50-0, 'Birch wood pyrolysate');
-insert into MP_PROHIBIDA values(84012-15-7, 'Birch wood pyrolysate');
-insert into MP_PROHIBIDA values(85251-66-7, 'Birch wood pyrolysate');
-insert into MP_PROHIBIDA values(85940-29-0, 'Birch wood pyrolysate');
-insert into MP_PROHIBIDA values(91745-85-6, 'Birch wood pyrolysate');
+CREATE TABLE PAGO(
+   id SERIAL PRIMARY KEY,
+   fecha DATE NOT NULL,
+   monto NUMERIC(9,2) NOT NULL
+);
 
-insert into MP_PROHIBIDA values(8006993, 'Chenopodium oil');
-insert into MP_PROHIBIDA values(8024111, 'Chenopodium oil');
-insert into MP_PROHIBIDA values(89997477, 'Chenopodium oil');
-insert into MP_PROHIBIDA values(8007009, 'Peru balsam');
-insert into MP_PROHIBIDA values(8013103, 'Cade oil');
-insert into MP_PROHIBIDA values(90046029, 'Cade oil');
-insert into MP_PROHIBIDA values(8022819, 'Boldo oil');
-insert into MP_PROHIBIDA values(84649967, 'Boldo oil');
-insert into MP_PROHIBIDA values(8023889, 'Costus root oil, absolute and concrete');
-insert into MP_PROHIBIDA values(90106551, 'Costus root oil, absolute and concrete');
-insert into MP_PROHIBIDA values(8024122, 'Verbena oil and absolute (Lippia citriodora Kunth.)');
-insert into MP_PROHIBIDA values(85116638, 'Verbena oil and absolute (Lippia citriodora Kunth.)');
-insert into MP_PROHIBIDA values(8046193, 'Styrax');
-insert into MP_PROHIBIDA values(8024019, 'Styrax');
-insert into MP_PROHIBIDA values(94891277, 'Styrax');
-insert into MP_PROHIBIDA values(94891288, 'Styrax');
-insert into MP_PROHIBIDA values(101227150, 'Styrax');
-insert into MP_PROHIBIDA values(8050097, 'Colophony');
-insert into MP_PROHIBIDA values(81152, 'Musk xylene');
-insert into MP_PROHIBIDA values(83669, 'Musk ambrette');
-insert into MP_PROHIBIDA values(84012204, 'Alantroot oil');
-insert into MP_PROHIBIDA values(97676352, 'Alantroot oil');
-insert into MP_PROHIBIDA values(84961580, 'Santolina oil');
-insert into MP_PROHIBIDA values(85085263, 'Massoia bark oil');
-insert into MP_PROHIBIDA values(87058, '4-Methyl-7-ethoxycoumarin');
-insert into MP_PROHIBIDA values(88299, 'Acetyl ethyl tetramethyl tetralin (AETT)');
-insert into MP_PROHIBIDA values(91225, 'Quinoline');
-
-insert into MP_PROHIBIDA values(92488, '6-Methylcoumarin');
-insert into MP_PROHIBIDA values(93992, 'Phenyl benzoate');
-insert into MP_PROHIBIDA values(94597, 'Safrole, Isosafrole and Dihydrosafrole');
-insert into MP_PROHIBIDA values(120581, 'Safrole, Isosafrole and Dihydrosafrole');
-insert into MP_PROHIBIDA values(94586, 'Safrole, Isosafrole and Dihydrosafrole');
-insert into MP_PROHIBIDA values(943884, 'Anisylidene acetone');
-insert into MP_PROHIBIDA values(98000, 'Furfuryl alcohol');
-insert into MP_PROHIBIDA values(98544, 'p-tert-Butylphenol');
-insert into MP_PROHIBIDA values(98953, 'Nitrobenzene');
-insert into MP_PROHIBIDA values(10031922, 'Esters of 2-Nonynoic acid (except Methyl octine carbonate)');
-insert into MP_PROHIBIDA values(10484329, 'Esters of 2-Octynoic acid (except Methyl heptine carbonate)');
-insert into MP_PROHIBIDA values(10519207, 'Esters of 2-Octynoic acid (except Methyl heptine carbonate)');
-insert into MP_PROHIBIDA values(8024008, 'Specification of Savin oil');
-insert into MP_PROHIBIDA values(90046041, 'Specification of Savin oil');
-insert into MP_PROHIBIDA values(68916949, 'Savin oil');
-insert into MP_PROHIBIDA values(90046030, 'Savin oil');
-
-insert into MP_PROHIBIDA values(90131434, 'Restriction and Specification of Tagetes patula and Tagetes minuta');
-insert into MP_PROHIBIDA values(8016840, 'Restriction and Specification of Tagetes patula and Tagetes minuta');
-insert into MP_PROHIBIDA values(91722291, 'Tagetes oil and absolute');
-insert into MP_PROHIBIDA values(8016840, 'Tagetes oil and absolute');
-insert into MP_PROHIBIDA values( 91770751, 'Tagetes oil and absolute');
-
-
---Ejemplo parainsertar productos en MP_PROHIBIDA
---INSERT INTO MP_PROHIBIDA VALUES(100, 'Algo prohibido xD');
 
 --Datos de prueba 
 
---INSERT INTO CONTINENTE VALUES(1, 'Europa');
+INSERT INTO PAIS VALUES(1, 'Francia', 'eu');
 
---INSERT INTO PAIS VALUES(1, 'Francia', 1);
+INSERT INTO PROVEEDOR VALUES(1, 'Juanito', 'hola@gmail.com', 2123987654, 'www.juanito.com', 1);
 
---INSERT INTO PROVEEDOR VALUES(1, 'Juanito', 'hola@gmail.com', 2123987654, 'www.juanito.com', 123, 'los proceres', 1071, 1);
+--Borrar tablas
+DROP TABLE PROVEEDOR;
+DROP TABLE PRODUCTOR;
+DROP TABLE CONTINENTE;
+DROP TABLE FORMA_ENVIO;
+DROP TABLE FORMA_PAGO;
+DROP TABLE PAIS;
+DROP TABLE CONTRATO;
