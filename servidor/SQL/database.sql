8e7752414154dcd4699a0d6b67349bee91194ab3
Black995 <54086005+Black995@users.noreply.github.com>
2020-07-02 12:18:43 -0400

    join de la info de contrato

--- servidor/SQL/database.sql
+++ servidor/SQL/database.sql
@@ -86,7 +86,7 @@ CREATE TABLE VAM_INGREDIENTE_ESENCIAS(
 );
 
 CREATE TABLE VAM_CONTRATOS(
-   id NUMERIC(3),
+   id SERIAL,
    id_prov INT,
    id_prod INT,
    fecha_emision DATE NOT NULL,
@@ -107,7 +107,7 @@ CREATE TABLE VAM_RENOVACIONES(
    id_prov INT,
    id_prod INT,
    fecha DATE NOT NULL,
-   CONSTRAINT fk_renov FOREIGN KEY(id_contrato, id_prov, id_prod) REFERENCES CONTRATOS(id, id_prov, id_prod) ON DELETE CASCADE,
+   CONSTRAINT fk_renov FOREIGN KEY(id_contrato, id_prov, id_prod) REFERENCES VAM_CONTRATOS(id, id_prov, id_prod) ON DELETE CASCADE,
    CONSTRAINT pk_renov PRIMARY KEY(id, id_contrato, id_prov, id_prod)
 );
 
@@ -172,6 +172,18 @@ INSERT INTO VAM_PROVEEDORES(nombre,email,telefono,pag_web,id_pais) VALUES('PROVE
 
 INSERT INTO VAM_PRODUCTORES(nombre,email,telefono,pag_web) VALUES('PRODUCTOR 1', 'hola@gmail.com', 2123555654, 'www.juanito3.com');
 INSERT INTO VAM_PRODUCTORES(nombre,email,telefono,pag_web) VALUES('PRODUCTOR 2', 'hola3@gmail.com', 2123354, 'www.juanito4.com');
+
+INSERT INTO VAM_INGREDIENTE_ESENCIAS(cas,id_prov,nombre,descripcion,taxonomia) VALUES(1234,1,'naranjita','soy naranjita','naranjus medium');
+
+INSERT INTO VAM_INGREDIENTE_ESENCIAS(cas,id_prov,nombre,descripcion,taxonomia) VALUES(132354,1,'manzanita','soy manzanita','manzanis tilum');
+
+INSERT INTO VAM_CONTRATOS(id_prov,id_prod,fecha_emision,exclusividad) VALUES(1,1,'10-10-2020',DEFAULT);
+
+INSERT INTO VAM_CONTRATOS(id_prov,id_prod,fecha_emision,exclusividad) VALUES(4,1,'10-19-2020',DEFAULT);
+
+
+INSERT INTO VAM_MP_C VALUES(1,1,1,1234,1);
+INSERT INTO VAM_MP_C VALUES(3,4,1,132354,1);
 
 --Borrar tablas
 DROP TABLE VAM_PROVEEDORES;
--- servidor/models/ContratoModel.js
+++ servidor/models/ContratoModel.js
@@ -6,19 +6,12 @@ class ContratoModel{
   getContratosSiendoProv(id_prov){
 		return new Promise((resolve, reject) => {
       
-      database.query('SELECT * FROM VAM_CONTRATOS WHERE id_prov=$1', [id_prov])
+			database.query('SELECT a.*, b.* FROM vam_contratos a, vam_ingrediente_esencias b, VAM_MP_C c INNER JOIN vam_contratos ON c.id_contrato=$1', [id_prov])
 			  .then(function(response){
 				  const prods = response.rows
 				  resolve(prods)
 			  })
         .catch(e => console.error(e.stack))
-        //Como consultar los ingredientes, de acuerdo a la entidad VAM_MP_C
-        database.query('SELECT * FROM VAM_INGREDIENTE_ESENCIAS WHERE id_prov=$1', [id_prov])
-			  .then(function(response){
-				  const prods = response.rows
-				  resolve(prods)
-			  })
-			  .catch(e => console.error(e.stack))
 		})
   }
   
